e8357faca3b89c97ffd11e8453fd9eb2c8636ed2
Dao Hoang Son <daohoangson@gmail.com>
2020-10-19 11:57:25 +0700

    Add more tests for `VndBottomSheet`

--- test/vnd_bottom_sheet_test.dart
+++ test/vnd_bottom_sheet_test.dart
@@ -67,4 +67,95 @@ void main() async {
 
     await screenMatchesGolden(tester, 'vnd_bottom_sheet/auto_zeros');
   });
+
+  testWidgets('handles taps', (tester) async {
+    final controller = VndEditingController();
+    var result;
+
+    await tester.pumpWidget(materialAppWrapper()(Builder(
+      builder: (context) => RaisedButton(
+        child: Text('RaisedButton'),
+        onPressed: () async {
+          result = await showModalBottomSheet(
+            builder: (_) => VndBottomSheet(controller: controller),
+            context: context,
+          );
+        },
+      ),
+    )));
+
+    await tester.tap(find.text('RaisedButton'));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.bySemanticsLabel('1'));
+    expect(controller.vnd, equals(1000));
+
+    await tester.tap(find.bySemanticsLabel('2'));
+    expect(controller.vnd, equals(12000));
+
+    await tester.tap(find.bySemanticsLabel('3'));
+    expect(controller.vnd, equals(123000));
+
+    await tester.tap(find.bySemanticsLabel('4'));
+    expect(controller.vnd, equals(1234));
+
+    await tester.tap(find.bySemanticsLabel('5'));
+    expect(controller.vnd, equals(12345));
+
+    await tester.tap(find.bySemanticsLabel('6'));
+    expect(controller.vnd, equals(123456));
+
+    await tester.tap(find.bySemanticsLabel('7'));
+    expect(controller.vnd, equals(1234567));
+
+    await tester.tap(find.bySemanticsLabel('8'));
+    expect(controller.vnd, equals(12345678));
+
+    await tester.tap(find.bySemanticsLabel('9'));
+    expect(controller.vnd, equals(123456789));
+
+    await tester.tap(find.bySemanticsLabel('000'));
+    expect(controller.vnd, equals(123456789000));
+
+    await tester.tap(find.bySemanticsLabel(String.fromCharCode(127)));
+    expect(controller.vnd, equals(12345678900));
+
+    expect(result, isNull);
+    await tester.tap(find.bySemanticsLabel('OK'));
+    await tester.pumpAndSettle();
+    expect(result, equals(12345678900));
+  });
+
+  testWidgets('disables autoZeros', (tester) async {
+    final controller = VndEditingController.fromValue(
+        VndEditingValue(autoZeros: true, rawValue: 100));
+    await tester.pumpWidget(
+        materialAppWrapper()(VndBottomSheet(controller: controller)));
+
+    expect(controller.autoZeros, isTrue);
+    expect(controller.vnd, equals(100000));
+
+    expect(find.text('100'), findsOneWidget);
+    expect(find.text(',000'), findsOneWidget);
+
+    await tester.drag(find.text(',000'), Offset(0, -100));
+    await tester.pumpAndSettle();
+
+    expect(controller.autoZeros, isFalse);
+    expect(controller.vnd, equals(100));
+  });
+
+  testWidgets('toggles isSelected', (tester) async {
+    final controller = VndEditingController(vnd: 100000);
+    await tester.pumpWidget(
+        materialAppWrapper()(VndBottomSheet(controller: controller)));
+
+    expect(controller.isSelected, isFalse);
+
+    await tester.tap(find.text('100,000'));
+    expect(controller.isSelected, isTrue);
+
+    await tester.tap(find.text('100,000'));
+    expect(controller.isSelected, isFalse);
+  });
 }
